9f8331ce9e51147572588cbbfc587eac13c0e182
Matt <magin3191@gmail.com>
2018-01-08 10:00:16 -0700

    fetch

--- Fetch/main.js
+++ Fetch/main.js
@@ -7,10 +7,19 @@ function plainFetch() {
     return response.json();
   }).then(function(json) {
     // so any additional json processing must happen in another then
-    console.log(json.data.children[0]);
+    console.log(json.data.children[1]);
   });
 }
 
 plainFetch();
+
+async function asyncFetch(){
+  let response = await fetch('https://www.reddit.com/r/boulder.json')
+  let json = await response.json()
+  console.log(json.data.children[0]);
+
+}
+
+
 
 // How can we use async / await to rewrite the function above?
